f54a39f314969425db8feb8e41466c903b1240df
Aleksandr Sarkisov <oleksandr.sarkisov@onereach.com>
2024-10-01 17:01:57 +0300

    feat: create sorting and categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import { Container, Title } from "@/components/shared";
+import { TopBar } from "@/components/shared/top-bar";
+import React from "react";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Home
-      </h1>
-    </div>
-  );
+  return <>
+    <Container className="mt-10">
+      <Title text="All pizzas" size="lg" className="font-extrabold" />
+
+
+
+    </Container>
+    <TopBar />
+    <div className="h-[3000px]"></div>
+  </>
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,2 +1,6 @@
+export { SortPopup } from './sort-popup'
+
+export { Categories } from './categories'
+export { Title } from './title'
 export { Header } from './header'
 export { Container } from './container'
